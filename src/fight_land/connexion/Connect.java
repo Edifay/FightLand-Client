619c269ea0ff65484554f248986e5764f160e202
Edifay <59512940+Edifay@users.noreply.github.com>
2020-08-24 16:25:50 +0200

    Update Connect.java

--- src/fight_land/connexion/Connect.java
+++ src/fight_land/connexion/Connect.java
@@ -6,16 +6,16 @@ import java.net.UnknownHostException;
 import networkAPI.Communication;
 
 public class Connect {
-	
+
 	public static Communication com;
-	
+
 	public static final String ipName = "localhost";
 	public static final int port = 7630;
-	
+
 	public static InetAddress ip;
-	
+
 	public static MenuRequestManager manager = null;
-	
+
 	public static void connect() {
 		try {
 			ip = InetAddress.getByName(ipName);
@@ -30,7 +30,7 @@ public class Connect {
 			System.out.println("Connected");
 			manager = new MenuRequestManager(com);
 			new Thread(manager).start();
-			
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
